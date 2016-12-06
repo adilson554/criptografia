42b06ee0eab7d3d1ff131810719a654d521b5746
adilson554 <adilson554@gmail.com>
2016-12-06 16:58:02 -0200

    Update criptografia.c

--- criptografia.c
+++ criptografia.c
@@ -6,41 +6,68 @@ int main(){
    int i;
    int j;
    int k;
-   int tamanhotexto;
-   int tamanhomensagem;
+   int l;
+   int m;
+   int ntable;
+   int nmessage;
+   int nvetor;
    bool ver;
    k = 0;
-//declarando e atribuindo valores no vetor de char
+//declarando e atribuindo valores nos vetores.
+  printf("=====================================\n");
+  printf("=====================================\n");
+  printf("         Criptografia\n");
+  printf("=====================================\n");
 
-   char texto[28] ={' ','A','B','C','D','E','F','G',
+   char table[28] ={' ','A','B','C','D','E','F','G',
                     'H','I','J','K','L','M','N','O',
                     'P','Q','R','S','T','U','V','W',
                     'X','Y','Z'};
-   char mensagem[100] = "ADILSON FRANCISCO";
+   char message[100];
+   printf("Entre com a mensagem em letras maiúsculas e sem acentos: \n");
+   scanf("%[^\n]s", message);
+   setbuf(stdin, NULL);
+   printf("\n");
+   printf("Você digitou: %s\n",message);
    int vetor[100];
    
-   tamanhotexto = strlen(texto);
-   tamanhomensagem = strlen(mensagem);
+   ntable = strlen(table);
+   nmessage = strlen(message);
 
-  printf("\n%d\n\n",tamanhotexto);
-  printf("\n%d\n\n",tamanhomensagem);
-  printf("\n%s\n",texto);
-  //printf("\n%s\n",mensagem);
-
-//
+  printf("=====================================\n");
+  printf("=====================================\n");
+  printf(" A mensagem convertida pela tabela\n");
+  printf("=====================================\n");
 
 
-for(j=0;j<tamanhomensagem;j = j+1){
-   for(i=0;i<tamanhotexto;i=i+1){
-      ver = (mensagem[j]==texto[i]);
+for (j = 0; j < nmessage; j = j + 1) {
+   for (i = 0; i < ntable; i = i + 1) {
+      ver = (message[j] == table[i]);
       if(ver == 1){
-         printf("%d\n",i);
+         printf("%d ",i);
          vetor[k] = i;
-         k = k+1;
+         k = k + 1;
          }
    }}
 
+printf("\n-----------------------------\n");
+printf("=====================================\n");
+  printf("=====================================\n");
+  printf(" MENSAGEM CONVERTIDA E CRIPTOGRAFADA (falta) \n");
+  printf("=====================================\n");
 
- 
+printf("\n-----------------------------\n");
+printf("=====================================\n");
+  printf("=====================================\n");
+  printf(" mensagem descriptografada: \n");
+  printf("=====================================\n");
+
+
+for(i=0;i<nmessage;i=i+1){
+   m = vetor[i];
+   printf("%c",table[m]);
+}
+printf("\n-----------------------------\n");
+
    return 0;
 }
